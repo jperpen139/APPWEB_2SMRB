0e2a844a8144d59f13d014eebf45f1bca6738115
jperpen139 <jperpen139@g.educaand.es>
2025-02-14 12:27:50 +0100

    guardado9

--- proyectos/color-flipper/js/script.js
+++ proyectos/color-flipper/js/script.js
@@ -1,11 +1,12 @@
-const color=["Red", "Blue", "Green", "Yellow", "Orange", "Purple", "Pink", "Brown", "Black", "White", "Gray", "Cyan", "Magenta", "Lime", "Teal", "Indigo", "Violet", "Gold", "Silver", "Beige"]
+document.addEventListener("DOMContentLoaded", function() {
+    const color = ["Red", "Blue", "Green", "Yellow", "Orange", "Purple", "Pink", "Brown", "Black", "White", "Gray", "Cyan", "Magenta", "Lime", "Teal", "Indigo", "Violet", "Gold", "Silver", "Beige"];
 
-    
+    const cambiar_color = document.querySelector(".boton__clickme");
+    const cambiar_texto = document.querySelector(".color-cambiar__valor");
 
-
-const cambiar_color=document.getElementsByClassName("boton__clickme")
-
-cambiar_color.addEventListener("click", function(){
-    const Colorrandom = color[Math.floor(Math.random() * color.length)];
-    document.body.style.backgroundColor = Colorrandom;
+    cambiar_texto,cambiar_color.addEventListener("click", function() {
+        const Colorrandom = color[Math.floor(Math.random() * color.length)];
+        document.body.style.backgroundColor = Colorrandom;
+        document.getElementsByClassName(".color-cambiar__valor").textContent= Colorrandom
+    });
 });
