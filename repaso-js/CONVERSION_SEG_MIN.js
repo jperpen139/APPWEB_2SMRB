555b376e1cd0f5d835bd53a83ccaa8a641c76de7
jperpen139 <jperpen139@g.educaand.es>
2025-05-14 11:35:50 +0200

    js ej finalizado

--- repaso-js/CONVERSION_SEG_MIN.js
+++ repaso-js/CONVERSION_SEG_MIN.js
@@ -1,1 +1,8 @@
 const segundos= parseInt(prompt("Escriba una cantidad de segundos: "))
+
+const minutos_segundos= segundos%60
+const segundos_minutos= segundos/60
+const minuto_entero= Math.trunc(segundos_minutos)
+
+
+alert(segundos+" segundos son "+minuto_entero +" minutos y "+ minutos_segundos+" segundos")
