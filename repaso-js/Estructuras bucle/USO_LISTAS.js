979fd802ef8b5ea2fcbb7c4890dab419463ee2d3
jperpen139 <jperpen139@g.educaand.es>
2025-05-20 16:02:12 +0200

    1º actividad terminada

---
+++ repaso-js/Estructuras bucle/USO_LISTAS.js
@@ -0,0 +1,45 @@
+
+for(let i=0;i <10;i++){
+    console.log(i)
+}
+
+
+for(let i=4;i<11;i++){
+    console.log(i)
+}
+
+for(let i=-6;i<0;i++){
+    console.log(i)
+}
+
+for(let i=-56;i<-49;i++){
+    console.log(i)
+}
+
+for(let i=1;i<18;i=i+=2){
+    console.log(i)
+}
+
+for(let i=-6;i<11;i=i+=2){
+    console.log(i)
+}
+
+for(let i=100;i<1001;i=i+=100){
+    console.log(i)
+}
+
+for(let i=10;i<4;i=i-=1){
+    console.log(i)
+}
+
+for(let i=-50;i<-57;i++){
+    console.log(i)
+}
+
+for(let i=17;i<0;i=i-=2){
+    console.log(i)
+}
+
+for(let i=500;i<-1;i=i-=100){
+    console.log(i)
+}
