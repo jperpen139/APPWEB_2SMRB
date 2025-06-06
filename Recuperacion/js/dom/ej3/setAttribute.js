ed080f6fd6d357d458759cd13b831bbe1fd95283
jperpen139 <jperpen139@g.educaand.es>
2025-06-06 10:11:41 +0200

    EJ 3 JS DOM terminado

---
+++ Recuperacion/js/dom/ej3/setAttribute.js
@@ -0,0 +1,6 @@
+const cambiar_img= document.querySelector(".boton")
+const imagen= document.querySelector(".imagen")
+
+cambiar_img.addEventListener("click",function(){
+    imagen.setAttribute("src","..//ej3/img/imagen2.jpg")
+})
